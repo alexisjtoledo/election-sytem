7a1c3960e487e42a8b9d8bc0aa0d199f238662a0
Alexis Toledo <alexis.toledo@live.com>
2022-07-11 21:39:32 +0200

    [ADD] Persistence of the state

--- src/app/election/electionReducer.js
+++ src/app/election/electionReducer.js
@@ -1,27 +1,43 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, current } from "@reduxjs/toolkit";
 import election from "../../data/election.json";
+
+/**
+ *  Checks if there's any persisted data in the browser
+ *  and selects a blank state if there aren't any.
+ */
+export const loadState = () => {
+    const preservedState = localStorage.getItem("state");
+    if (preservedState) {
+        return JSON.parse(preservedState);
+    } else {
+        return {
+            electionName: election.name,
+            blankBallots: election.ballots,
+            users: election.users,
+            login: {
+                isUserLogged: false,
+                userId: null,
+                userBallots: [],
+            },
+            results: {
+                totalVotes: 0,
+                castedVotes: [],
+            },
+        };
+    }
+};
+
+const currentState = loadState();
 
 export const electionSlice = createSlice({
     name: "election",
 
-    initialState: {
-        electionName: election.name,
-        blankBallots: election.ballots,
-        users: election.users,
-        login: {
-            isUserLogged: false,
-            userId: null,
-            userBallots: [],
-        },
-        results: {
-            totalVotes: 0,
-            castedVotes: [],
-        },
-    },
+    initialState: currentState,
 
     reducers: {
         setLogin: (state, action) => {
             state.login = action.payload;
+            localStorage.setItem("state", JSON.stringify(current(state)));
         },
         castVote: (state, action) => {
             state.login.userBallots = state.login.userBallots.map((ballot) =>
@@ -37,6 +53,7 @@ export const electionSlice = createSlice({
                     ? { ...user, ballots: state.login.userBallots }
                     : user,
             );
+            localStorage.setItem("state", JSON.stringify(current(state)));
         },
     },
 });
